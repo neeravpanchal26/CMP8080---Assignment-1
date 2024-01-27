dc85049241ad17009a94acad4fabf297570ea5f1
Neerav Panchal <neeravpanchal26@gmail.com>
2024-01-27 22:11:00 +0000

    Create a method named Divide, and ensure that it accepts a get request and two decimal parameters.
    Return the quotient of the two parameters. Remember that ordering is important!

--- CalculatorApi/calculator.cs
+++ CalculatorApi/calculator.cs
@@ -21,5 +21,11 @@ namespace MyApp.Namespace
         {
             return number1 * number2;
         }
+        public decimal Divide(decimal number1 = 0, decimal number2 = 0)
+        {
+            decimal bigNumber = number1 > number2 ? number1 : number2;
+            decimal smallNumber = number1 < number2 ? number1 : number2;
+            return bigNumber / smallNumber;
+        }
     }
 }
