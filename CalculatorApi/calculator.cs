939e09a419253d2f1aac3f844921c6a0457aa558
Neerav Panchal <neeravpanchal26@gmail.com>
2024-01-27 22:03:39 +0000

    Create a method named Add, and ensure that it accepts a get request and two decimal parameters.
    Return the sum of the two parameters.

--- CalculatorApi/calculator.cs
+++ CalculatorApi/calculator.cs
@@ -7,5 +7,9 @@ namespace MyApp.Namespace
     [ApiController]
     public class calculator : ControllerBase
     {
+        public decimal Add(decimal number1 = 0, decimal number2 = 0)
+        {
+            return number1 + number2;
+        }
     }
 }
