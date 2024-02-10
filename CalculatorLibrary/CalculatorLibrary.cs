20d412292cf86f3271ff7d91fe4a29f4e71e6898
Neerav Panchal <neeravpanchal26@gmail.com>
2024-02-10 19:14:45 +0000

    Move the logic from the API Controllers into the class library, Reference the class library from the unit tests project, Reference the class library from the API project

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -1,4 +1,4 @@
-namespace CalculatorLibrary;
+namespace CalculatorLibrary
 {
     public class Calculator
     {
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLibrary;
+
+namespace CalculatorLibrary.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator = new Calculator();
+    }
+}
